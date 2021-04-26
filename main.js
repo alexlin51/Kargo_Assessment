80894403110d13e42d6d9d4f5e84b48c61eea9ce
Alex Lin <alexander88lin@yahoo.com>
2021-04-26 13:37:11 -0700

    Completed Solution

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 function ConvertNums(input){
-    return
-  }
+    const store = {
+        0: "Zero",
+        1: "One",
+        2: "Two",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine"
+    };
+    
+    let output = "";
+    input.forEach(num => {
+        let n = num.toString().split("");
+            n.forEach(digit => {
+            output += store[digit];
+        });
+        output += ",";
+    });
+    return output;
+}
   
   
-  let input1 = [3,25,209];
-  console.log(ConvertNums(input1))
+let input1 = [3,25,209];
+console.log(ConvertNums(input1));
   
-  let input2 = [10, 300, 5];
-  console.log(ConvertNums(input2))
+let input2 = [10, 300, 5];
+console.log(ConvertNums(input2));
