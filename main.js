a1a5080a64634cf27d2165891ebc24f7ee152369
Alex Lin <alexander88lin@yahoo.com>
2021-04-26 13:35:10 -0700

    Setting up problem inputs and function

---
+++ main.js
@@ -0,0 +1,10 @@
+function ConvertNums(input){
+    return
+  }
+  
+  
+  let input1 = [3,25,209];
+  console.log(ConvertNums(input1))
+  
+  let input2 = [10, 300, 5];
+  console.log(ConvertNums(input2))
